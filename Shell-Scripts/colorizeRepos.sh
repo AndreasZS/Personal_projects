ff3a4410282b5847fd9483549f13131712b32518
Andreas Z. Singer <azsinger@bitbucket.org>
2020-09-28 14:05:08 -0400

    updated comments

--- Shell-Scripts/colorizeRepos.sh
+++ Shell-Scripts/colorizeRepos.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Credit for this to numerous stackexchange posts and a response by unix.stackexchange user mikeserv
+# link: https://unix.stackexchange.com/questions/148342/fine-tune-ls-colors-for-directory-suffixes
+
 # Bash script to detect git repositories
 # Can be run from any location
+
+# =========================================================================================================================
+# WARNING:
+# This works by setting the sticky bits of directories so that users other than the one who created them CANNOT delete them.
+# The ls output color of these directories is then based on the value of 'st' in the LS_COLORS environment variable
+# =========================================================================================================================
 
 # Function to return list of git repositories
 # When run from a directory, checks all files/directories within to see if they contain a '.git' repo base
@@ -27,12 +36,12 @@ function list_repos {
     eval $final="'${output[@]}'"
 }
 
+# This will set sticky bits for the repo directories
+# Other users will NOT be able to delete them
+# Its color in ls output will change according to the LS_COLORS environment variable
+# Based on the color-code values for 'st'
+# To undo, run chmod o-t <directory-name> or following directions below to use 'undo_sticky' function
 function set_sticky {
-    # This will set sticky bits for the repo directories
-    # Other users will NOT be able to delete them
-    # Its color in ls output will change according to the LS_COLORS environment variable
-    # Based on the color-code values for 'st'
-    # To undo, run chmod o-t <directory-name>
     local git_repos=$1
     for r in ${git_repos[@]}; do
         chmod o+t $r
