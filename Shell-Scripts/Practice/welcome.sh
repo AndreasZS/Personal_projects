a48d1bf8c6e50861cd295e0cf814b43273ef0c6b
Andreas Z. Singer <azsinger@bitbucket.org>
2020-10-14 12:22:58 -0400

    Removed Shell-Scripts folder, created a Bash_Scripts repo

--- Shell-Scripts/Practice/welcome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Example greeting script
-
-greeting="Welcome"
-user=$(whoami)
-day=$(date +%A)
-
-echo "$greeting back $user! Today is $day!"
-echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
